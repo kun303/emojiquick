dbb2859772c41c299c3f0992681ff7b297a715e2
Ethan <kkunjia@gmail.com>
2016-06-25 22:42:51 +0800

    project commit

---
+++ sample/src/main/java/com/ethan/emojiquick/sample/EmojiSampleActivity.java
@@ -0,0 +1,15 @@
+package com.ethan.emojiquick.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ethan.emojiquick.sample.R;
+
+public class EmojiSampleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emoji_sample);
+    }
+}
